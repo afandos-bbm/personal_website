0c70d7f7c8bb3abd3d614e9456a4d9934460a406
Fan2 <alejandrofan2@gmail.com>
2023-01-29 22:20:44 +0100

    fix format

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,19 +1,19 @@
 module.exports = {
-  extends: ["standard", "plugin:astro/recommended"],
+  extends: ['standard', 'plugin:astro/recommended'],
   rules: {
-    "no-tabs": "off",
-    indent: "off",
-    "space-before-function-paren": "off",
+    'no-tabs': 'off',
+    indent: 'off',
+    'space-before-function-paren': 'off'
   },
   overrides: [
     {
-      files: ["*.astro"],
-      parser: "astro-eslint-parser",
+      files: ['*.astro'],
+      parser: 'astro-eslint-parser',
       parserOptions: {
-        parser: "@typescript-eslint/parser",
-        extraFileExtensions: [".astro"],
+        parser: '@typescript-eslint/parser',
+        extraFileExtensions: ['.astro']
       },
-      rules: {},
-    },
-  ],
-};
+      rules: {}
+    }
+  ]
+}
