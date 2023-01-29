b17d09c9bd8ff2b26a3b1c0a8b264001c3b0af0b
Fan2 <alejandrofan2@gmail.com>
2023-01-29 22:16:40 +0100

    fix format

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./src/**/*.{html,js,astro}'],
+  content: ["./src/**/*.{html,js,astro}"],
   theme: {
-    extend: {}
+    extend: {},
   },
-  plugins: []
-}
+  plugins: [],
+};
