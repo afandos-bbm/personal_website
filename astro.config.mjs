c64b04257c5b8dccc613950ccd061a52d9675581
Fan2 <alejandrofan2@gmail.com>
2023-01-31 18:01:19 +0100

    add astro sitemap

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,9 @@
 import { defineConfig } from 'astro/config'
-
-// https://astro.build/config
 import tailwind from '@astrojs/tailwind'
-
-// https://astro.build/config
 import robotsTxt from 'astro-robots-txt'
+import sitemap from '@astrojs/sitemap'
 
-// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), robotsTxt()],
+  integrations: [tailwind(), sitemap(), robotsTxt()],
   site: 'https://alejandrofan2.dev'
 })
