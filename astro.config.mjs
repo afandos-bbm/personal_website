3c8e2ccde0cfc937534708a235ea9b3f9c98690c
Fan2 <alejandrofan2@gmail.com>
2023-01-31 17:58:02 +0100

    add astro-robots-txt

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,8 +4,10 @@ import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
+import robotsTxt from 'astro-robots-txt'
+
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind()],
+  integrations: [tailwind(), robotsTxt()],
   site: 'https://alejandrofan2.dev'
-  // base: '/personal_website'
 })
