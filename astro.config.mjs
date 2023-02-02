63ad6d440eeb6a2a47fee501c7258b4d41d7aea4
Fan2 <alejandrofan2@gmail.com>
2023-02-02 11:08:14 +0100

    add react config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 import { defineConfig } from 'astro/config'
+import react from '@astrojs/react'
 import tailwind from '@astrojs/tailwind'
 import robotsTxt from 'astro-robots-txt'
 import sitemap from '@astrojs/sitemap'
 
+// https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), sitemap(), robotsTxt()],
+  integrations: [react(), tailwind(), sitemap(), robotsTxt()],
   site: 'https://alejandrofan2.dev'
 })
